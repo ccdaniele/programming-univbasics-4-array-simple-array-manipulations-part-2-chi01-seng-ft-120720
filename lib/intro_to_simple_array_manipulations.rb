468d479d432e14face7dd7b3dd6bc0b12cfce496
Daniel Calderon <danieleduardocald@gmail.com>
2020-11-21 21:43:08 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -11,4 +11,4 @@ end
 def using_insert ("Python")
    list_of_programming_languages = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"]
     another_language = "Python"
-    list_of_programming_languages.insert(another_languages)
+    list_of_programming_languages.insert(4, "Python")
