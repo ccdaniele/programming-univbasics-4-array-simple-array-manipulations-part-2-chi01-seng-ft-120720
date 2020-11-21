4b06652053c1577d1ed9ea9280303278dbb9ab66
Daniel Calderon <danieleduardocald@gmail.com>
2020-11-21 21:53:02 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,8 +1,7 @@
 
-def using_concat(my_favorite_things, more_favs)
-  my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
-  more_favs = ["sports cars", "flatiron school"]
-  my_favorite_things.concat(more_favs)
+def using_concat(array, brray)
+  
+  array.concat(brray)
 
 end
 
